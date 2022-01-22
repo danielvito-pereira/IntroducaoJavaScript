6baaaf230f295651c0428233f00f16c78187c04c
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2022-01-22 20:27:30 -0300

    final

--- javascriptweb/asscts/js/estrutura.js
+++ javascriptweb/asscts/js/estrutura.js
@@ -1,1 +1,16 @@
-console.log("Olá")
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber =0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    if ( currentNumber <0) {
+        currentNumber.style.color = 'red'
+    }
+}
